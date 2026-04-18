afd918c4902a8b2896366f0a6521da650eeb3ac4
hawkstein <liamhawks+liamhawks@gmail.com>
2026-04-18 16:26:03 +0100

    add basic dust particles

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 @export var base_velocity: float = 64.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dust_particles: GPUParticles2D = $DustParticles
+@onready var left_marker: Marker2D = $LeftMarker
+@onready var right_marker: Marker2D = $RightMarker
+@onready var up_marker: Marker2D = $UpMarker
+@onready var down_marker: Marker2D = $DownMarker
 
 var speed := 1.0
 var current_direction:= Vector2i.ZERO
@@ -17,6 +22,7 @@ func _physics_process(_delta: float) -> void:
 	_read_input()
 	_update_sprite()
 	velocity = Vector2(current_direction) * speed * base_velocity
+	_update_particles()
 	move_and_slide()
 
 func _read_input() -> void:
@@ -59,6 +65,22 @@ func _update_sprite() -> void:
 		animated_sprite.flip_h = false
 	elif current_direction == Vector2i.DOWN:
 		animated_sprite.play("down")
+
+func _update_particles() -> void:
+	if speed >= 2.0:
+		dust_particles.emitting = true
+	else:
+		dust_particles.emitting = false
+		return
+	
+	if current_direction == Vector2i.UP:
+		dust_particles.position = up_marker.position
+	elif current_direction == Vector2i.LEFT:
+		dust_particles.position = left_marker.position
+	elif current_direction == Vector2i.RIGHT:
+		dust_particles.position = right_marker.position
+	elif current_direction == Vector2i.DOWN:
+		dust_particles.position = down_marker.position
 
 func reset() -> void:
 #	TODO: clear any data for this attempt
