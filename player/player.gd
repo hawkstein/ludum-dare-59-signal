9f433596685146ad3009841e2a78674bd8be7683
hawkstein <liamhawks+liamhawks@gmail.com>
2026-04-18 13:50:24 +0100

    add basic animation sprite for player

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @export var tile_map: TileMapLayer
-@export var base_velocity: float = 64.0 
+@export var base_velocity: float = 64.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var speed := 1.0
 var current_direction:= Vector2i.ZERO
@@ -13,6 +15,7 @@ func _physics_process(_delta: float) -> void:
 	if not active:
 		return
 	_read_input()
+	_update_sprite()
 	velocity = Vector2(current_direction) * speed * base_velocity
 	move_and_slide()
 
@@ -44,6 +47,18 @@ func _read_input() -> void:
 	
 	current_direction = desired_direction
 	# TODO: add speed up or brake code i.e if you tap the same direction speed up, tap the opposite to brake
+
+func _update_sprite() -> void:
+	if current_direction == Vector2i.UP:
+		animated_sprite.play("up")
+	elif current_direction == Vector2i.LEFT:
+		animated_sprite.play("sideways")
+		animated_sprite.flip_h = true
+	elif current_direction == Vector2i.RIGHT:
+		animated_sprite.play("sideways")
+		animated_sprite.flip_h = false
+	elif current_direction == Vector2i.DOWN:
+		animated_sprite.play("down")
 
 func reset() -> void:
 #	TODO: clear any data for this attempt
