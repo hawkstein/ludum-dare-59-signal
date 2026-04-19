d31b3f1cebea4042883a120c6a1b9fd4892f1cc6
hawkstein <liamhawks+liamhawks@gmail.com>
2026-04-19 08:36:25 +0100

    add spawner system for mibs

--- main.gd
+++ main.gd
@@ -8,8 +8,9 @@ extends Node2D
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 @onready var message_layer: MessageLayer = $MessageLayer
 
-
+const MAN_IN_BLACK = preload("uid://d2ag0ing1neyp")
 const TRANSMITTER = preload("uid://c7v1iqui8bx7a")
+
 var _transmitter_location := Vector2.ZERO
 var _transmitters_found := 0
 
@@ -76,3 +77,76 @@ func _on_transmitter_found() -> void:
 
 func _switch_to_win() -> void:
 	get_tree().change_scene_to_file("res://narrative/win.tscn")
+
+
+func _on_spawn_timer_timeout() -> void:
+	if player.current_direction == Vector2i.ZERO:
+		return
+	var mib_position := _find_spawn_position()
+	if mib_position != Vector2.ZERO:
+		var mib := MAN_IN_BLACK.instantiate()
+		add_child(mib)
+		mib.position = mib_position
+		mib.add_to_group("mib")
+		print("spawned man in black")
+	else:
+		print("could not find spawn position for mib")
+
+func _find_spawn_position(
+) -> Vector2:
+	var viewport_rect := get_viewport().get_visible_rect()
+	var canvas_transform := get_canvas_transform()
+	var world_rect := Rect2(
+		-canvas_transform.origin / canvas_transform.get_scale(),
+		viewport_rect.size / canvas_transform.get_scale()
+	)
+	
+	var margin: float = 64.0
+	var max_attempts := 5
+	for _i in max_attempts:
+		var point := _point_in_front_of_player(world_rect, player.current_direction, margin)
+		var tile_coords := tile_map_layer.local_to_map(tile_map_layer.to_local(point))
+
+		if _tile_without_collision(tile_coords) and _away_from_transmitter(point):
+			return point
+
+	return Vector2.ZERO
+
+
+func _point_in_front_of_player(
+	rect: Rect2, facing: Vector2i, margin: float
+) -> Vector2:
+	var pos := Vector2.ZERO
+	
+	match facing:
+		Vector2i.RIGHT:
+			pos.x = rect.end.x + margin + randf() * margin
+			pos.y = randf_range(rect.position.y, rect.end.y)
+		Vector2i.LEFT:
+			pos.x = rect.position.x - margin - randf() * margin
+			pos.y = randf_range(rect.position.y, rect.end.y)
+		Vector2i.DOWN:
+			pos.y = rect.end.y + margin + randf() * margin
+			pos.x = randf_range(rect.position.x, rect.end.x)
+		Vector2i.UP:
+			pos.y = rect.position.y - margin - randf() * margin
+			pos.x = randf_range(rect.position.x, rect.end.x)
+	
+	return pos
+
+
+func _tile_without_collision(coords: Vector2i) -> bool:
+	var tile_data := tile_map_layer.get_cell_tile_data(coords)
+	if tile_data == null:
+		return false
+	
+	var physics_layers_count := tile_map_layer.tile_set.get_physics_layers_count()
+	for i in physics_layers_count:
+		if tile_data.get_collision_polygons_count(i) > 0:
+			return false
+	
+	return true
+
+func _away_from_transmitter(mib_position: Vector2) -> bool:
+	var min_distance := 96 # 64 radius of mib + 1 tile 32px
+	return _transmitter_location.distance_to(mib_position) > min_distance
--- world/man_in_black.gd
+++ world/man_in_black.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var despawn_timer: Timer = $DespawnTimer
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("reset"):
@@ -8,3 +9,16 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _switch_to_start_again() -> void:
 	get_tree().change_scene_to_file("res://narrative/start-again.tscn")
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	despawn_timer.start(randi_range(5, 10))
+
+
+func _on_despawn_timer_timeout() -> void:
+	print("despawning man in black")
+	call_deferred("queue_free")
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	despawn_timer.stop()
